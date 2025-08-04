fe9616ff9ce83613faa59b01190397d2f2ed36df
Binary <130375355+smartcomps@users.noreply.github.com>
2025-08-04 16:26:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig(({ mode }) => {
       // The `define` property injects your API keys as environment variables
       // during the build process, so they are available in your code.
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify("AIzaSyCv6ri2MdIaWH5-J5sOd-ElTjCYqg0PUm0"),
+        'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyCv6ri2MdIaWH5-J5sOd-ElTjCYqg0PUm0")
       },
 
       // The `resolve` property allows you to use a '@' alias for cleaner imports.
